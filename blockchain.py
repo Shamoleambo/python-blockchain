82fa4c45c2c366c182ac9d82b6b20473fa955bc9
Tiago Gomes <tidgomes@gmail.com>
2023-08-17 10:57:25 -0300

    using default values

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,16 @@
-blockchain = [[1]]
+blockchain = []
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(value):
-    blockchain.append([get_last_blockchain_value(), value])
+def add_value(value, last_transaction=[1]):
+    blockchain.append([last_transaction, value])
 
 
 add_value(1.17)
-add_value(123.21)
-add_value(-14)
+add_value(123.21, get_last_blockchain_value())
+add_value(-14, get_last_blockchain_value())
 
 print(blockchain)
