70d77520a7c5f7d80d0e7499b7f141d6df098a20
Tiago Gomes <tidgomes@gmail.com>
2023-08-17 10:54:15 -0300

    create function that returns something

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,16 @@
 blockchain = [[1]]
 
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
 def add_value(value):
-    blockchain.append([blockchain[-1], value])
-    print(blockchain)
+    blockchain.append([get_last_blockchain_value(), value])
+
+
+add_value(1.17)
+add_value(123.21)
+add_value(-14)
+
+print(blockchain)
